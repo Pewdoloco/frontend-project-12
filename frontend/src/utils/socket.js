f2b8c052c8a9e62e84d48d2201a697306483f8a5
Pewdoloco <ersovlevd@gmail.com>
2025-06-21 15:50:54 +0300

    socket

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io({
+const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:5001', {
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
-  auth: (cb) => {
+  auth: cb => {
     const token = localStorage.getItem('token');
     cb({ token });
   },
