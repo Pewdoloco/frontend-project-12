c1b2971e350670ccb33d5e292f3b61f213d693e1
Pewdoloco <ersovlevd@gmail.com>
2025-06-24 19:39:11 +0300

    add log code

--- frontend/src/utils/initCI.js
+++ frontend/src/utils/initCI.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 
 export default function initCI() {
-    console.log('CI mode:', import.meta.env.VITE_CI);
+  console.log('CI mode:', import.meta.env.VITE_CI);
   if (import.meta.env.VITE_CI === 'true') {
+    console.log('CI init: creating admin.');
     axios.post('/api/v1/signup', {
       username: 'admin',
       password: 'admin',
+    }).then(() => {
+      console.log('админ создан');
     }).catch((e) => {
-      if (e?.response?.status !== 409) {
-        console.error('CI init admin user failed:', e);
+      if (e?.response?.status === 409) {
+        console.log('админ уже существует');
+      } else {
+        console.error('Не удалось создать админа', e.message);
       }
     });
   }
