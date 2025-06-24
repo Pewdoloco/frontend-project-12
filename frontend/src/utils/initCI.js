2676b959aa94f18eb919ced09150eb082b8c2c55
Pewdoloco <ersovlevd@gmail.com>
2025-06-24 19:32:42 +0300

    update CI

--- frontend/src/utils/initCI.js
+++ frontend/src/utils/initCI.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default function initCI() {
+    console.log('CI mode:', import.meta.env.VITE_CI);
   if (import.meta.env.VITE_CI === 'true') {
     axios.post('/api/v1/signup', {
       username: 'admin',
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_CI': JSON.stringify(process.env.VITE_CI || ''),
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-      },
-      '/socket.io': {
-        target: 'http://localhost:5001',
-        ws: true,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname);
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_CI': JSON.stringify(env.VITE_CI),
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5001',
+          changeOrigin: true,
+        },
+        '/socket.io': {
+          target: 'http://localhost:5001',
+          ws: true,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    sourcemap: true,
-  },
+    build: {
+      sourcemap: true,
+    },
+  }
 });
