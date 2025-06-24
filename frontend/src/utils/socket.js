c6b0053aabca7599a33d2068a1fcad45cb9fb0c6
Pewdoloco <ersovlevd@gmail.com>
2025-06-25 02:28:41 +0300

    fix deploy

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:5001', {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ['websocket'],
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
@@ -8,7 +9,7 @@ const socket = io(import.meta.env.VITE_API_URL || 'http://localhost:5001', {
     const token = localStorage.getItem('token');
     cb({ token });
   },
-  transports: ['websocket', 'polling'],
 });
+
 
 export default socket;
