e073a2be2fb7ccc18eabfc82441c80f52533c91d
Pewdoloco <ersovlevd@gmail.com>
2025-06-25 14:52:35 +0300

    fix lint ProtectedRoute, RemoveChannelModal

--- frontend/src/components/RemoveChannelModal.jsx
+++ frontend/src/components/RemoveChannelModal.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Modal, Button } from 'react-bootstrap';
-import { removeChannel } from '../store';
-import { useTranslation } from 'react-i18next';
-import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from 'react-redux'
+import { Modal, Button } from 'react-bootstrap'
+import { removeChannel } from '../store'
+import { useTranslation } from 'react-i18next'
+import { toast } from 'react-toastify'
 
 function RemoveChannelModal({ show, onHide, channelId }) {
-  const dispatch = useDispatch();
-  const { loading } = useSelector(state => state.chat);
-  const { t } = useTranslation();
-  const state = useSelector(state => state.chat);
+  const dispatch = useDispatch()
+  const { loading } = useSelector(state => state.chat)
+  const { t } = useTranslation()
+  const state = useSelector(state => state.chat)
 
   return (
     <Modal show={show} onHide={onHide} centered>
@@ -27,11 +26,12 @@ function RemoveChannelModal({ show, onHide, channelId }) {
           variant="danger"
           onClick={async () => {
             try {
-              const channelName = state.channels.find(c => c.id === channelId)?.name;
-              await dispatch(removeChannel(channelId)).unwrap();
-              toast.success(t('toast.channelRemoved', { name: channelName }));
-              onHide();
-            } catch {
+              const channelName = state.channels.find(c => c.id === channelId)?.name
+              await dispatch(removeChannel(channelId)).unwrap()
+              toast.success(t('toast.channelRemoved', { name: channelName }))
+              onHide()
+            }
+            catch {
               // Ошибка обрабатывается в chatSlice.js через toast.error
             }
           }}
@@ -41,7 +41,7 @@ function RemoveChannelModal({ show, onHide, channelId }) {
         </Button>
       </Modal.Footer>
     </Modal>
-  );
+  )
 }
 
-export default RemoveChannelModal;
+export default RemoveChannelModal
