4c9d4ee28e09ac8859605b78f43543a146809444
Pewdoloco <ersovlevd@gmail.com>
2025-06-25 19:28:51 +0300

    fix __dirname vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 /* eslint-env node */
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
 export default defineConfig(({ mode }) => {
+  const __dirname = fileURLToPath(new URL('.', import.meta.url))
   const env = loadEnv(mode, __dirname)
   return {
     plugins: [react()],
