42e45bd0f9918975b66a7051934622e48f86db8e
Pewdoloco <ersovlevd@gmail.com>
2025-06-25 16:50:04 +0300

    fix lint upload-source-maps 2 , vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+/* eslint-env node */
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname);
+  const env = loadEnv(mode, __dirname)
   return {
     plugins: [react()],
     define: {
@@ -25,4 +26,4 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
     },
   }
-});
+})
