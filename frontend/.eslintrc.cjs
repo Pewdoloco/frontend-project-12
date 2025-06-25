0aaf2d885e53ed51bdb77f0d7029983a9a89d73a
Pewdoloco <ersovlevd@gmail.com>
2025-06-25 17:11:53 +0300

    add fix lint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off"
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
