534c5a76f0907ea20bc836c6b7de216735a24249
Pewdoloco <ersovlevd@gmail.com>
2025-06-27 02:51:12 +0300

    fix sq

--- frontend/src/utils/registerAdminCI.js
+++ frontend/src/utils/registerAdminCI.js
@@ -2,9 +2,16 @@ import axios from 'axios'
 
 const registerAdminCI = async () => {
   try {
+    const username = import.meta.env.VITE_ADMIN_USERNAME
+    const password = import.meta.env.VITE_ADMIN_PASSWORD
+
+    if (!username || !password) {
+      throw new Error('Admin credentials are not set')
+    }
+
     await axios.post('/api/v1/signup', {
-      username: 'admin',
-      password: 'admin',
+      username,
+      password,
     })
   }
   catch (error) {
